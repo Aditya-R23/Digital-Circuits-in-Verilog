5694be53e1368f58e3515d035e5d969db6d78bcd
Aditya R <226676975+Aditya-R23@users.noreply.github.com>
2026-03-31 22:26:00 +0530

    XOR gate

---
+++ 01_combinational_circuits/01_basic_gates/05_xor/xorg_tb.v
@@ -0,0 +1,20 @@
+module xorg_tb;
+reg a, b;
+wire y;
+xorg xor1 (.a(a), .b(b), .y(y));
+
+initial begin
+    $monitor("a = %b, b = %b, y = %b", a, b, y);
+    $dumpfile("xorg_tb.vcd");
+    $dumpvars(0, xorg_tb);
+    end
+
+initial begin
+    a = 1'b0; b = 1'b0; #10;
+    a = 1'b0; b = 1'b1; #10;
+    a = 1'b1; b = 1'b0; #10;
+    a = 1'b1; b = 1'b1; #10;
+    $finish;
+    end
+
+endmodule
