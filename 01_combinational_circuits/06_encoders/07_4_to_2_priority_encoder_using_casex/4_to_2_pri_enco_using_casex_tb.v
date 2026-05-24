8aa938e15e5df0b4967baf347684d33136acaf2d
Aditya R <226676975+Aditya-R23@users.noreply.github.com>
2026-05-24 16:55:34 +0530

    Prin Encoder

---
+++ 01_combinational_circuits/06_encoders/07_4_to_2_priority_encoder_using_casex/4_to_2_pri_enco_using_casex_tb.v
@@ -0,0 +1,20 @@
+module pri_enco_4_to_2_using_casex_tb();
+    reg [3:0] in;
+    wire [1:0] out;
+
+    pri_enco_4_to_2_using_casex enco (in, out);
+
+    initial begin
+        $monitor("in = %b, out = %b", in, out);
+        $dumpfile("pri_enco_4_to_2_using_casex_tb.vcd");
+        $dumpvars(0, pri_enco_4_to_2_using_casex_tb);
+    end
+
+    initial begin
+        in = 4'b0001; #10; 
+        in = 4'b0010; #10;
+        in = 4'b0100; #10;
+        in = 4'b1000; #10;
+        $finish;
+    end
+endmodule
